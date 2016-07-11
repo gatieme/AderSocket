117943ee675be37af42fa43761ce3cdb4e52db16
gatieme <gatieme@163.com>
2016-07-11 16:06:29 +0800

    test

--- study/codes/9/9-4oneshot.cpp
+++ study/codes/9/9-4oneshot.cpp
@@ -33,11 +33,17 @@ int setnonblocking( int fd )
     return old_option;
 }
 
+/*  将fd上的EPOLLIN和EPOLLET事件注册到epollfd指示的epoll内核事件表中
+ *  参数oneshot用来指定是否注册fd上的EPOLLONESHOT事件
+ * */
 void addfd( int epollfd, int fd, bool oneshot )
 {
     epoll_event event;
     event.data.fd = fd;
     event.events = EPOLLIN | EPOLLET;
+    /*  对于注册了EPOLLONESHOT时间的文件描述符
+     *  操作系统最多触发其上注册的一个可读, 可写或者异常事件
+     *  且只触发一次  */
     if( oneshot )
     {
         event.events |= EPOLLONESHOT;
@@ -46,6 +52,10 @@ void addfd( int epollfd, int fd, bool oneshot )
     setnonblocking( fd );
 }
 
+/*  重置fd上的事件, 这样操作后, 尽管fd上的EPOLLONESHOT事件被注册后,
+ *  但是操作系统仍然会触发fd上的EPOLLIN事件
+ *  且只触发一次
+ * */
 void reset_oneshot( int epollfd, int fd )
 {
     epoll_event event;
@@ -54,6 +64,7 @@ void reset_oneshot( int epollfd, int fd )
     epoll_ctl( epollfd, EPOLL_CTL_MOD, fd, &event );
 }
 
+/*  工作线程  */
 void* worker( void* arg )
 {
     int sockfd = ( (fds*)arg )->sockfd;
@@ -61,6 +72,7 @@ void* worker( void* arg )
     printf( "start new thread to receive data on fd: %d\n", sockfd );
     char buf[ BUFFER_SIZE ];
     memset( buf, '\0', BUFFER_SIZE );
+    /*  循环读取sockfd上的数据, 直到遇见EAGAIN错误  */
     while( 1 )
     {
         int ret = recv( sockfd, buf, BUFFER_SIZE-1, 0 );
@@ -82,10 +94,13 @@ void* worker( void* arg )
         else
         {
             printf( "get content: %s\n", buf );
+            /*  休眠5s, 模拟数据处理过程    */
             sleep( 5 );
         }
     }
     printf( "end thread receiving data on fd: %d\n", sockfd );
+
+    return NULL;
 }
 
 int main( int argc, char* argv[] )
@@ -134,6 +149,12 @@ int main( int argc, char* argv[] )
     epoll_event events[ MAX_EVENT_NUMBER ];
     int epollfd = epoll_create( 5 );
     assert( epollfd != -1 );
+    /*   注意, 监听套接字listen上不能注册EPOLLONESHOT事件,
+     *   否则应用程序只能处理一个客户端连接
+     *   因为由于EPOLLONESHOT被设
+     *   置后续的客户端连接请求将不再触发listenfd的EPOLLIN
+     *
+     * */
     addfd( epollfd, listenfd, false );
 
     while( 1 )
